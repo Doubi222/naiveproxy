1571159aad90d21ed30b80d53a23cf22ce70b06a
klzgrad <kizdiv@gmail.com>
2022-11-10 13:17:52 +0800

    Update allocator_shim_default_dispatch_to_partition_alloc.cc

--- src/base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
+++ src/base/allocator/partition_allocator/shim/allocator_shim_default_dispatch_to_partition_alloc.cc
@@ -300,7 +300,6 @@ void PartitionAllocSetCallNewHandlerOnMallocFailure(bool value) {
 }
 
 void* PartitionMalloc(const AllocatorDispatch*, size_t size, void* context) {
-  fprintf(stderr, "PartitionMalloc %zu\n", size);
   partition_alloc::ScopedDisallowAllocations guard{};
   return Allocator()->AllocWithFlagsNoHooks(
       g_alloc_flags, MaybeAdjustSize(size),
